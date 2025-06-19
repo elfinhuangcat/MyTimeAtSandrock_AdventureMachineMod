852b0930c02faa53368704bdeba752410155caca
elfinhuangcat <elfinhuangcat@gmail.com>
2025-06-18 21:42:07 -0700

    Initial commit for adventure machine mod to always win money.

---
+++ MyTimeAtSandrock_AdventureMachineMod/AdventureMachineAlwaysWinMoney.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Pathea.AdventureMachineNs;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+[BepInPlugin(NAMESPACE, NAME, VERSION)]
+public class AdventureMachineAlwaysWinMoney : BaseUnityPlugin
+{
+    public const string NAMESPACE = "elfinhuangcat.sandrockmods.advmachinecheat";
+    public const string NAME = "AdventureMachineAlwaysWinMoney";
+    public const string VERSION = "1.0.0";
+
+    public static ManualLogSource Log;
+    /// <summary>
+    /// Load the mod and patch the necessary methods using Harmony.
+    /// </summary>
+    private void Awake()
+    {
+        Log = Logger;
+
+        var harmony = new Harmony(NAMESPACE);
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    /// <summary>
+    /// Patcthe method that checks if a bet was successful in the Adventure Machine game,
+    /// and reward the first bet placed by the player.
+    /// </summary>
+    [HarmonyPatch(typeof(BattleInfo), "IsBetSuccess")]
+    static class IsBetSuccess_Patch
+    {
+        // 1) __instance: the BattleInfo object
+        // 2) out int rewardGold: the original out parameter
+        // 3) ref bool __result: intercepts the return value
+        static bool Prefix(BattleInfo __instance,
+                           out int rewardGold,
+                           ref bool __result)
+        {
+            // Get the field "betInfos", which should contain the bets that the player put down. 
+            FieldInfo fiBetInfos = AccessTools.Field(typeof(BattleInfo), "betInfos");
+            List<BetInfo> betInfos = (List<BetInfo>)fiBetInfos.GetValue(__instance);
+
+            // Check if the player has placed any bets
+            if (betInfos != null || betInfos.Count > 0)
+            {
+                // Reward based on the first bet
+                for (int i = 0; i < betInfos.Count; i++)
+                {
+                    if (betInfos[i] != null && betInfos[i].betCount > 0)
+                    {
+                        rewardGold = betInfos[i].rewardGold * betInfos[i].betCount;
+                        __result = true;  // Indicate that the bet was successful
+                        return false;  // skip the original method entirely
+                    }
+                }
+            }
+            rewardGold = 0;  // No bets, no reward
+            return true;  // allow the original method to run
+        }
+    }
+}
+
---
+++ MyTimeAtSandrock_AdventureMachineMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AdventureMachineAlwaysWinMoney")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdventureMachineAlwaysWinMoney")]
+[assembly: AssemblyCopyright("Copyright © elfinhuangcat 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d86c31c-7e22-43a4-abea-b89fb44d10d6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
